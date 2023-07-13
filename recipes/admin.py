be605bdb6bd4ffa4b57585d9a79d6a0f58e6ec87
FernandaAssuncao <fernandaassuncaosa@gmail.com>
2023-07-12 22:22:05 -0300

    Adiconando recipe e configurando rotas.

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Recipe
 
 # Register your models here.
 
@@ -8,5 +8,9 @@ class CategoryAdmin(admin.ModelAdmin):
     ...
 
 
-admin.site.register(Category, CategoryAdmin)
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
 
+
+admin.site.register(Category, CategoryAdmin)
