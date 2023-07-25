e7e02643ddfba1acc7ffee904e7cd5e2aa2a41c3
FernandaAssuncao <fernandaassuncaosa@gmail.com>
2023-07-24 23:18:29 -0300

    Retornando http 404 para pagínas de categorias que não existem.

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from django.http import Http404
 from .models import Recipe
 
 # Create your views here.
@@ -14,8 +15,11 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    if not recipes:
+        raise Http404('Not Found!')
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name}',
     })
 
 
